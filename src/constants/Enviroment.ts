76197cbf2a08037290f99bf31158911ae07536bc
Franco <chipanasiguas@gmail.com>
2023-09-18 18:57:44 -0500

    constantes de variables de entorno

---
+++ src/constants/Enviroment.ts
@@ -0,0 +1,10 @@
+import { Env } from '../utils';
+
+export class Enviroment {
+	public static SERVER_PORT = Env.getNumber('SERVER_PORT');
+	public static DB_PORT = Env.getNumber('DB_PORT');
+	public static DB_HOST = Env.getString('DB_HOST');
+	public static DB_USER = Env.getString('DB_USER');
+	public static DB_PASS = Env.getString('DB_PASS');
+	public static DB_NAME = Env.getString('DB_NAME');
+}
