4e467b360ace3a889ee3e2c40462e6215b2cc339
Franco <chipanasiguas@gmail.com>
2023-09-18 18:59:57 -0500

    usando variables de entorno, en el puerto del servidor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Server } from './app/Server';
+import { Enviroment } from './constants';
 
 export const main = async () => {
-	const app = new Server(4000);
+	const app = new Server(Enviroment.SERVER_PORT);
 	await app.start();
 };
